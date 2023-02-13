7ceb7be6d61374847a0049e7e25cb670f738612c
natan borges <natanmelo5@hotmail.com>
2023-02-13 10:27:10 -0300

    Verificacao de vitoria.

--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -11,6 +11,12 @@ while (jogoRolando)
 {
     PrintarJogo();
     JogaPlayer(jogador);
+    if (VerificarGanhador(jogador))
+    {
+        PrintarJogo();
+        Console.WriteLine(string.Format("Jogador {0}({1}) venceu!!!", jogador, Enum.GetName(typeof(Simbolos), jogador)));
+        jogoRolando = false;
+    }
     jogador = (jogador + 1) % 2;
 
 }
@@ -30,19 +36,22 @@ void PrintarJogo()
 };
 void JogaPlayer(int jogador)
 {
-    Console.WriteLine("Digite a coluna selecionada: ");
+    var JogadorSimbolo = Enum.GetName(typeof(Simbolos), jogador);
+    Console.WriteLine(string.Format("Jogador ({0}) Digite a coluna selecionada: ",JogadorSimbolo));
     var coluna = Convert.ToInt16(Console.ReadLine()) -1;
-    Console.WriteLine("Digite a linha selecionada: ");
-    var linha = Convert.ToInt16(Console.ReadLine()) -1;
     if (coluna < 0 || coluna > 2)
     {
         Console.WriteLine("Coluna invalida");
         JogaPlayer(jogador);
+        return;
     }
+    Console.WriteLine(string.Format("Jogador ({0}) Digite a linha selecionada: ", JogadorSimbolo));
+    var linha = Convert.ToInt16(Console.ReadLine()) -1;
     if( linha < 0 || linha > 2)
     {
         Console.WriteLine("Linha invalida");
         JogaPlayer(jogador);
+        return;
     }
 
     if (matriz[linha,coluna] == "null")
@@ -53,10 +62,57 @@ void JogaPlayer(int jogador)
     {
         Console.WriteLine("Posição já foi escolhido por um jogador");
         JogaPlayer(jogador);
+        return;
     }
-        
+}
+bool VerificarGanhador(int jogador)
+{
+    if (VerificarGanhadorHorizontal(jogador) || VerificarGanhadorVertical(jogador) || VerificarGanhadorDiagonal(jogador))
+    {
+        return true;
+    }
+    else return false;
+}
+bool VerificarGanhadorHorizontal (int jogador)
+{
+    bool jogadorGanhou = false;
+    var jogadorSimbolo = Enum.GetName(typeof(Simbolos), jogador);
+    for (var linha = 0; linha < 3; linha++)
+    {
+        if (matriz[linha, 0] == jogadorSimbolo && matriz[linha, 1] == jogadorSimbolo && matriz[linha, 2] == jogadorSimbolo)
+        {
+            jogadorGanhou = true;
+        }
+    }
+    return jogadorGanhou;
+}
+bool VerificarGanhadorVertical(int jogador)
+{
+    bool jogadorGanhou = false;
+    var jogadorSimbolo = Enum.GetName(typeof(Simbolos), jogador);
+    for (var coluna = 0; coluna < 3; coluna++)
+    {
+        if (matriz[0, coluna] == jogadorSimbolo && matriz[1, coluna] == jogadorSimbolo && matriz[2, coluna] == jogadorSimbolo)
+        {
+            jogadorGanhou = true;
+        }
+    }
+    return jogadorGanhou;
+}
+bool VerificarGanhadorDiagonal(int jogador)
+{
+    bool jogadorGanhou = false;
+    var jogadorSimbolo = Enum.GetName(typeof(Simbolos), jogador);
+    if (matriz[0, 0] == jogadorSimbolo && matriz[1, 1] == jogadorSimbolo && matriz[2, 2] == jogadorSimbolo)
+    {
+        jogadorGanhou = true;
+    }
+    if (matriz[0, 2] == jogadorSimbolo && matriz[1, 1] == jogadorSimbolo && matriz[2, 0] == jogadorSimbolo)
+    {
+        jogadorGanhou = true;
+    }
 
-
+    return jogadorGanhou;
 }
 enum Simbolos
 {
